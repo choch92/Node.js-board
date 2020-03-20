3c1047ef5e236e5c2b43aeb0da3f44e1165c084e
choch92 <54798173+choch92@users.noreply.github.com>
2020-03-20 13:58:05 +0900

    Node.js(게시판 만들기)
    VSCode로 Node서버를 사용하여 게시판 만들기

---
+++ js/table.js
@@ -0,0 +1,8 @@
+// 테이블 정의 : ECMAScript 2015 클래스, 상속 지원
+class Table{
+    // 생성자안에 맴버변수 정의
+    constructor(){
+        this.table = document.createElement("table");
+        this.tr = document.createElement("tr");
+    }
+}
